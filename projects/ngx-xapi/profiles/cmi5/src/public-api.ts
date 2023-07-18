f1e73463858723eac93e939d92355c20027c985f
István Rátkai <istvan@berrycloud.co.uk>
2023-07-18 09:12:09 +0100

    Add basic cmi5 profile (#21)

---
+++ projects/ngx-xapi/profiles/cmi5/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ngx-xapi/profile/cmi5
+ */
+
+export * from './lib/cmi5-statements';
+export * from './lib/launch-data';
+export * from './lib/launch';
+export * from './lib/learner-preferences';
