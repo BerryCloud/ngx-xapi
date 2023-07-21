83b1d2ea2fbe1e3a8b1aa67225b25ed3e5fd6237
István Rátkai <istvan@berrycloud.co.uk>
2023-07-21 09:48:51 +0100

    sss

---
+++ projects/ngx-xapi/course/src/lib/xapi-course.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { XapiCourseService } from './xapi-course';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot([])],
+  exports: [RouterModule],
+})
+export class LanguageMapPipeModule {}
